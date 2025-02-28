58ce5d0a34bbc8b661479314ac1b7ac51e5a02fa
far <99890691+xofvr@users.noreply.github.com>
2025-02-28 16:18:17 +0000

    refactor: removed unused json import in views

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
-import json
 import os
 import logging
 import pickle
