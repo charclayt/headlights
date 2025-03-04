02405d43604469873568e103348e20668ba5ce77
jqw2-douet <jack2.douet@live.uwe.ac.uk>
2025-03-04 11:33:18 +0000

    feature: basic user login

--- desd/urls.py
+++ desd/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
     path("myapp/", include("myapp.urls")),
     path("", RedirectView.as_view(url="myapp/", permanent=True)),
