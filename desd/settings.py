cc1a1746b0e22660f64d4fa2115421375074d24e
far <farhan.shakeel@icloud.com>
2025-03-19 20:06:06 +0000

    fix: changed to relative paths for the index not found test case

--- desd/settings.py
+++ desd/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "desd.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join("/app", "templates")],  # Changed from "/shared/templates"
+        "DIRS": [os.path.join(BASE_DIR, "templates")],  # I changed from "DIRS": [os.path.join("/app", "templates")] to this for relative paths:"
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
