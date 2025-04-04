c040c618af52c301b0699b750893429f11012565
t2-akbulut <troy2.akbulut@live.uwe.ac.uk>
2025-04-04 07:24:46 +0100

    Removed unused and untested case conversion function

--- ml_project/ml_app/utility/CaseConversion.py
+++ ml_project/ml_app/utility/CaseConversion.py
@@ -2,7 +2,4 @@ def to_pascal(string: str):
     string = string.replace("_", " ")
     string = ''.join(word[0].upper() + word[1:] for word in string.split())
     return string.replace(" ", "")
-
-def to_snake(string: str):
-    string = to_pascal(string)
-    return ''.join(['_'+char.lower() if char.isupper() else char for char in string]).lstrip('_')
+    
