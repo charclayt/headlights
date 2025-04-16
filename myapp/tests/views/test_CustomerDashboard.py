abe7e4e954ba3ec045b6c77c084314c43a83a408
jqw2-douet <jack2.douet@live.uwe.ac.uk>
2025-04-16 18:31:28 +0100

    Revert "fix: missing keys in correct order"
    This reverts commit 8aa49023a1a1e324c5d4cfb8e4c60dc5360d63b9.

--- myapp/tests/views/test_CustomerDashboard.py
+++ myapp/tests/views/test_CustomerDashboard.py
@@ -72,7 +72,7 @@ class CustomerDashboardTest(BaseViewTest, TestCase):
         with self.assertRaises(ValueError) as context:
             create_uploaded_record(record)
 
-        self.assertEqual(str(context.exception), "Missing required keys in record: {'prediction', 'model_id'}")
+        self.assertEqual(str(context.exception), "Missing required keys in record: {'model_id', 'prediction'}")
 
 
     def test_get_view(self):
