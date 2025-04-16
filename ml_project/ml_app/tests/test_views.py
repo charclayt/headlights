cfd28000cfbf32bef452021e7e2e48c0466b2b4c
Jack-P1 <jackpilbeam101@gmail.com>
2025-04-16 11:18:24 +0100

    ml service success test cases

--- ml_project/ml_app/tests/test_views.py
+++ ml_project/ml_app/tests/test_views.py
@@ -1,6 +1,6 @@
 # Temporary test until front-end functionality added to MLDashboard to predict
 # TODO: move test to myapp/tests/views/test_MLDashboardPage.py when predict FE implemented.
-
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 import logging
 import os
@@ -8,7 +8,48 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from unittest.mock import patch
 
-from ml_app.models import PredictionModel
+from ml_app.models import PredictionModel, PreprocessingStep, PreprocessingModelMap
+
+class ModelListTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = 'api/models/'
+
+        filepath = '/shared/media/models/limited_model.pkl'
+        if os.getenv('GITHUB_ACTIONS') == 'true':
+            filepath = '../dataset/limited_model.pkl'
+
+        self.model = PredictionModel.objects.create(
+            model_id=1,
+            model_name='test_model',
+            model_type='default',
+            notes='',
+            filepath=filepath)
+        
+        step = PreprocessingStep.objects.create(
+            preprocessing_step_id = 1,
+            preprocess_name = 'create_days_between_col'
+        )
+
+        PreprocessingModelMap.objects.create(
+            preprocessing_model_map_id = 1,
+            preprocessing_step_id=step,
+            model_id=self.prediction_model
+        )
+
+        logging.disable(logging.ERROR)
+    
+    def tearDown(self):
+        logging.disable(logging.NOTSET)
+        return super().tearDown()
+
+    def test_get_model_success(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual('test_model', response.data['models_list']['name'])
+        self.assertEqual('create_days_between_col', response.data['models_list']['preprocessingSteps'])
+
 
 class ModelPredictTestCase(TestCase):
     
@@ -61,3 +102,36 @@ class ModelPredictTestCase(TestCase):
             self.assertEqual(response.status_code, status.HTTP_200_OK)
             self.assertIn('data', response.data)
             self.assertEqual(response.data['data']['prediction'], {'result': 0.95})
+
+class ModelUploadTestCase(TestCase):
+    
+    def setUp(self):
+        self.client = APIClient()
+        self.url = 'api/upload-model/'
+
+        self.test_file = SimpleUploadedFile(
+            "test_model.pkl", b"fake-model-content", content_type="application/octet-stream"
+        )
+        
+        self.preprocessingStep = PreprocessingStep.objects.create(
+            preprocessing_step_id = 1,
+            preprocess_name = 'create_days_between_col'
+        )
+
+        logging.disable(logging.ERROR)
+
+    def test_model_upload_success(self):
+        data = {
+            'model_name': 'TestModel',
+            'notes': 'test notes',
+            'selected_steps': [self.preprocessingStep.preprocessing_step_id]
+        }
+
+        response = self.client.post(self.url, data={
+            **data,
+            'model_file': self.test_file
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['status'], 'success')
+        self.assertTrue(PredictionModel.objects.filter(model_name='TestModel').exists())
