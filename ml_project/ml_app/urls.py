91b0e65f1fe35b9215f7fab8e9a6fe5867134499
t2-akbulut <troy2.akbulut@live.uwe.ac.uk>
2025-04-28 14:20:57 +0100

    Added page and URL for editing models

--- ml_project/ml_app/urls.py
+++ ml_project/ml_app/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path("api/models/", views.ModelListView.as_view(), name="models_list"),
+    path("api/models/<int:model_id>/", views.ModelListView.as_view(), name="models_list"),
     path("api/upload-model/", views.UploadModelView.as_view(), name="upload_model"),
+    path("api/model/edit/<int:model_id>/", views.EditModelView.as_view(), name="model_edit"),
     path("api/model/predict/", views.ModelPredict.as_view(), name="model_predict"),
     path("health/", views.HealthCheckView.as_view(), name="health_check"),
 ]
