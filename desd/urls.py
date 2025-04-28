38285a02b9b2cedd476e29ce74a332ecbffc50bc
Jack-P1 <jackpilbeam101@gmail.com>
2025-04-28 15:07:23 +0100

    custom login auth to display verbose error with inactive account

--- desd/urls.py
+++ desd/urls.py
@@ -19,10 +19,12 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 from desd.settings import DEBUG
 from myapp.views.EngineerDashboardView import EngineerDashboardView
 from myapp.views.ErrorView import Error_400, Error_403, Error_404, Error_500
+from myapp.forms.CustomAuthenticationForm import CustomAuthenticationForm
 
 handler400 = Error_400.as_view()
 handler403 = Error_403.as_view()
@@ -30,7 +32,42 @@ handler404 = Error_404.as_view()
 handler500 = Error_500.as_view()
 
 urlpatterns = [
-    path("accounts/", include("django.contrib.auth.urls")),
+    # LOGIN/LOGOUT
+    path('accounts/login/', auth_views.LoginView.as_view(
+        template_name='registration/login.html', 
+        authentication_form=CustomAuthenticationForm
+    ), name='login'),
+    
+    path('accounts/logout/', auth_views.LogoutView.as_view(
+        template_name='registration/logged_out.html'
+    ), name='logout'),
+
+    # PASSWORD CHANGE (requires user to be logged in)
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(
+        template_name='registration/password_change_form.html'
+    ), name='password_change'),
+    
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='registration/password_change_done.html'
+    ), name='password_change_done'),
+
+    # PASSWORD RESET (email-based)
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html'
+    ), name='password_reset'),
+    
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'
+    ), name='password_reset_done'),
+    
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+    
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'
+    ), name='password_reset_complete'),
+
     path("admin/", admin.site.urls),
     
     # Root redirects to app/
